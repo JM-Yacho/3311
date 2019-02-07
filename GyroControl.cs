139afd4b07890f69bc95687ce5e0b837ebffb711
LuSukidi <lulusukidi@gmail.com>
2019-02-07 00:59:10 -0600

    Create GyroControl.cs

---
+++ GyroControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
