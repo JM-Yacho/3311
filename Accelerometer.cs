0806fc0731b6cb39f1c9e6dd344c56fa8add88c5
LuSukidi <lulusukidi@gmail.com>
2019-02-07 20:09:07 -0600

    Create Accelerometer.cs
    script for ball that makes it move with a device when it is on a flat surface and tilted

---
+++ Accelerometer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public bool isflat = true;
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        if (isflat)
+        {
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+
+        rigid.AddForce(tilt);
+    }
+}
