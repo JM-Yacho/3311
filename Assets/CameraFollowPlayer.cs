7df066b282a0f1c60261b38ec251207fe37abf6a
LuSukidi <lulusukidi@gmail.com>
2019-02-15 12:54:26 -0600

    For Unity

---
+++ Assets/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void Update()
+    {
+        transform.position = offset + player.transform.position;
+    }
+}
