846ee9f56d9e81f92bb80468bf57c075a6db6af7
JM <jmyach08@gmail.com>
2019-03-17 23:02:30 -0500

    Added "Island" folder with assets for first chapter. Added my level 3 design in progress.

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -4,63 +4,19 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    public Transform Target;
+    public GameObject player;
 
     private Vector3 offset;
-    private float distance = 5.0f;
-    private float yOff = 1.5f;
-
-    //Camera swiping variables
-    private Vector2 touchpos;
-    private float swipeRes = 100f;
-    private Vector3 desiredPos;
-    private float smoothspeed = 7.5f;
-
-
     
     // Start is called before the first frame update
     void Start()
     {
-        offset = new Vector3(0, yOff, -1f * distance);
+        offset = transform.position - player.transform.position;
     }
 
+    // LateUpdate is called after Update each frame
     void Update()
     {
-        
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
-        {
-            touchpos = Input.mousePosition;
-        }
-        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
-        {
-            float swipeforce = touchpos.x - Input.mousePosition.x;
-            if (Mathf.Abs(swipeforce) > swipeRes)
-            {
-                //There is a swipe!
-                if (swipeforce < 0) //Left swipe
-                    SlideCamera(true);
-                else
-                    SlideCamera(false);
-            }
-        }
-    }
-    private void FixedUpdate()
-    {
-        desiredPos = Target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, desiredPos, smoothspeed * Time.deltaTime);
-        transform.LookAt(Target.position + Vector3.up);
-    }
-
-    public void SlideCamera(bool left)
-    {
-        //If not left, then right by default
-        if (left)
-        {
-            offset = Quaternion.Euler(0, 90, 0) * offset;
-        }
-        else
-        {
-            offset = Quaternion.Euler(0, -90, 0) * offset;
-        }
+        transform.position = offset + player.transform.position;
     }
 }
---
+++ Assets/Scripts/platformAttach.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformAttach : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Player.transform.parent = null;
+    }
+}
