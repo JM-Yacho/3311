b7bc56134995651f4b9a43667b4b57838b3062cb
LuSukidi <lulusukidi@gmail.com>
2019-03-17 22:47:05 -0500

    added mechanics

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -4,19 +4,63 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    public GameObject player;
+    public Transform Target;
 
     private Vector3 offset;
+    private float distance = 5.0f;
+    private float yOff = 1.5f;
+
+    //Camera swiping variables
+    private Vector2 touchpos;
+    private float swipeRes = 100f;
+    private Vector3 desiredPos;
+    private float smoothspeed = 7.5f;
+
+
     
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = new Vector3(0, yOff, -1f * distance);
     }
 
-    // LateUpdate is called after Update each frame
     void Update()
     {
-        transform.position = offset + player.transform.position;
+        
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            touchpos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+        {
+            float swipeforce = touchpos.x - Input.mousePosition.x;
+            if (Mathf.Abs(swipeforce) > swipeRes)
+            {
+                //There is a swipe!
+                if (swipeforce < 0) //Left swipe
+                    SlideCamera(true);
+                else
+                    SlideCamera(false);
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        desiredPos = Target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, smoothspeed * Time.deltaTime);
+        transform.LookAt(Target.position + Vector3.up);
+    }
+
+    public void SlideCamera(bool left)
+    {
+        //If not left, then right by default
+        if (left)
+        {
+            offset = Quaternion.Euler(0, 90, 0) * offset;
+        }
+        else
+        {
+            offset = Quaternion.Euler(0, -90, 0) * offset;
+        }
     }
 }
--- Assets/Scripts/JumpMotor.cs
+++ Assets/Scripts/JumpMotor.cs
@@ -10,6 +10,7 @@ public class JumpMotor : MonoBehaviour
     private Collider playercollider;
     private float disttoground;
     private float jumpForce = 15.0f;
+    private bool is_tap, moved;
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +21,39 @@ public class JumpMotor : MonoBehaviour
         disttoground = playercollider.bounds.extents.y;
 
     }
-    bool isGrounded()
+    public bool IsGrounded()
     {
         return Physics.Raycast(transform.position, Vector3.down, disttoground + 0.1f);
     }
+    //Makes sure jumping and camera changing dont interfer with each other
+    public bool IsTap(Touch touch)
+    {
+        switch (touch.phase)
+        {
+            case TouchPhase.Began:
+                is_tap = true;
+                break;
+            case TouchPhase.Moved:
+                is_tap = false;
+                moved = true;
+                break;
+            case TouchPhase.Ended:
+                is_tap = true;
+                break;
+        }
+        if (moved == false && is_tap)
+            return true;
+        else
+            return false;
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
        
-            if(Input.touchCount == 1)
+            if(Input.touchCount > 0)
             {
-                if (Input.GetTouch(0).phase == TouchPhase.Began && isGrounded())
+                if (Input.GetTouch(0).phase == TouchPhase.Began && IsGrounded() && IsTap(Input.GetTouch(0)))
                 {
                 player.AddForce(0, jumpForce, 0);
                 }
