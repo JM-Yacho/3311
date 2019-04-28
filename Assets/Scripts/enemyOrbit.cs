bbab7adf407568ce2c06c2b4846d184a11ccf754
LuSukidi <lulusukidi@gmail.com>
2019-04-27 22:49:52 -0500

    Time save functional

---
+++ Assets/Scripts/enemyOrbit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyOrbit : MonoBehaviour
+{
+    public GameObject focalPoint;
+    private float speed = 75;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(focalPoint.transform.position, Vector3.down, speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/fallOff.cs
+++ Assets/Scripts/fallOff.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 public class fallOff : MonoBehaviour
 {
     public GameObject Player;
+    public Rigidbody rigid;
+    public Transform respawnpoint;
+    private void Start()
+    {
+        
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == Player)
         {
+            
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);        
         }
     }
