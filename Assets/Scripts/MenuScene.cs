280a8da7fc84461d8d70729a371b4e27af62377c
JM-Yacho <jmyach08@gmail.com>
2019-04-28 11:48:24 -0500

    Update MenuScene.cs

--- Assets/Scripts/MenuScene.cs
+++ Assets/Scripts/MenuScene.cs
@@ -6,31 +6,7 @@ using UnityEngine.UI;
 
 public class MenuScene : MonoBehaviour
 {
-    private CanvasGroup fadeGroup;
-    private float fadeinspeed = 2f;
 
-    
-
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        //Get the only canvas group in the scene
-        fadeGroup = FindObjectOfType<CanvasGroup>();
-
-        //Start with a white screen
-        fadeGroup.alpha = 1;
-
-       
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        //Fading in
-        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeinspeed;
-
-    }
 
     //Buttons
     public void onPlayclick()
